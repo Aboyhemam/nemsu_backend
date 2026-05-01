406def07fda8e147074bf40d0d3cbdae57fca153
Aboyhemam <markhemam@gmail.com>
2026-05-02 01:06:07 +0530

    5th commit

--- routes/getEvents.js
+++ routes/getEvents.js
@@ -2,6 +2,6 @@ const getEvents=require('../scripts/getEvents.js')
 const express=require('express');
 const router=express.Router();
 
-router.use('/get',getEvents);
+router.get('/get',getEvents);
 
 module.exports=router;
