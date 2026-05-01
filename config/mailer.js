c7f886948dd70d5e1127278673de7a626d4a5453
Aboyhemam <markhemam@gmail.com>
2026-05-02 05:24:14 +0530

    trying to fix mail service

--- config/mailer.js
+++ config/mailer.js
@@ -2,11 +2,13 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
-    user: process.env.EMAIL_USER,      // nesmuofficial@gmail.com
-    pass: process.env.EMAIL_PASS       // app password
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
   }
 });
 
-module.exports = transporter;
+module.exports=transporter;
