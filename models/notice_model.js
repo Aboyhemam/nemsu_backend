cd7d5f4bee1b3431e681f697041ab4c020aeac97
Aboyhemam <markhemam@gmail.com>
2026-05-07 20:58:18 +0530

    notice get2

--- models/notice_model.js
+++ models/notice_model.js
@@ -3,6 +3,6 @@ const db=require('mongoose')
 const noticeModel= new db.Schema({
     title:{type:String, required:true},
     fileUrl:{type:String, required: true}
-},{timestamp:true})
+},{timestamps:true})
 
 module.exports=db.model('Notice',noticeModel)
