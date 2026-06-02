b9a8d6ded3f66e819260e7f876bb19ae81534cc0
Aboyhemam <markhemam@gmail.com>
2026-06-02 11:57:46 +0530

    newmember3

--- routes/memberRoute.js
+++ routes/memberRoute.js
@@ -2,6 +2,15 @@ const app=require("express")
 const router=app.Router();
 const newMember=require("./../controller/createMember.js")
 const upload = require('../middleware/multer_setup.js');
-router.post("/add",upload.array("images"),newMember);
+router.post(
+  "/add",
+  upload.fields([
+    { name: "passportPhoto", maxCount: 1 },
+    { name: "payment_SS", maxCount: 1 },
+    { name: "student_sign", maxCount: 1 },
+    { name: "parent_sign", maxCount: 1 }
+  ]),
+  newMember
+);
 
 module.exports=router;
